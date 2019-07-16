7259bb2c4f50226af93d10ce16fec7929c94be97
Srinivas Gudla <gsrinivas37@gmail.com>
2019-07-16 12:26:44 -0400

    .

--- video_preview.php
+++ video_preview.php
@@ -1,6 +1,40 @@
+<?php
+$HDD_ROOT = "/mnt/hdd";
+$GATE_VIDEO_DIR = $HDD_ROOT."/GateVideos";
+$STAIRS_VIDEO_DIR = $HDD_ROOT."/StairsVideos";
 
-<html>
-<title>GateVideos (2019-07-15) (19hour)</title>
-<head><h1><center><a href="../../../">GateVideos</a>&nbsp<a href="../">(2019-07-15)</a> (19hour) </a></center></h1></head>
-<body>
-<h2><h2><div style="float: left"><a href="../18hour"> Previous</a> (18hour)</div><div style="float: right"><a href="../20hour"> Next</a> (20hour)</div><div style="margin: auto; width: 250px;"><a href="../../../GatePhotos/2019-07-15/19hour/persons">Photos</a>&emsp;&emsp;<a href="../../../StairsVideos/2019-07-15/19hour">Stairs</a></div></h2><h2><a href="./19.47.44-19.49.35[M][0@0][0].mp4">19.47.44-19.49.35[M][0@0][0].mp4</a> (54.55 MB)</h2><h2><a href="./19.09.55-19.10.19[M][0@0][0].mp4">19.09.55-19.10.19[M][0@0][0].mp4</a> (11.77 MB)</h2><h2><a href="./19.36.28-19.37.17[M][0@0][0].mp4">19.36.28-19.37.17[M][0@0][0].mp4</a> (24.37 MB)</h2><h2><a href="./19.23.31-19.23.54[M][0@0][0].mp4">19.23.31-19.23.54[M][0@0][0].mp4</a> (10.28 MB)</h2><h2><a href="./19.50.23-19.51.09[M][0@0][0].mp4">19.50.23-19.51.09[M][0@0][0].mp4</a> (21.74 MB)</h2></body></html>
+$CAMERA = $_GET["camera"];
+$DATE = $_GET["date"];
+$HOUR = $_GET["hour"];
+
+$int_hour = substr($HOUR,0,2);
+$prev_hour = sprintf("%'.02d",$int_hour-1);
+$next_hour = sprintf("%'.02d",$int_hour+1);
+$other_cam = $CAMERA=="Gate"? "Stairs" : "Gate";
+
+echo '<html>';
+echo "<title>$CAMERA Videos ($DATE) ($HOUR)</title>";
+echo "<head><h1><center><a href='../../../'>$CAMERA Videos</a>&nbsp<a href='../'>($DATE)</a> ($HOUR) </a></center></h1></head>";
+echo '<body>';
+echo '<h2><h2>';
+
+if($int_hour!="00"){
+    echo "<div style='float: left'><a href='./video_preview.php?camera=$CAMERA&date=$DATE&hour=$prev_hour'> Previous</a> (18hour)</div>";
+}
+if($int_hour!="23"){
+    echo "<div style='float: right'><a href='./video_preview.php?camera=$CAMERA&date=$DATE&hour=$next_hour'> Next</a> (20hour)</div>";
+}
+
+echo "<div style='margin: auto; width: 250px;'>";
+echo "<a href='./preview.php?camera=$CAMERA&date=$DATE&hour=$HOUR&tag=person'>Photos</a>&emsp;&emsp;<a href='./video_preview.php?camera=$other_cam&date=$DATE&hour=$HOUR'>$other_cam</a></div></h2>";
+
+$video_dir = $CAMERA=="Gate"? $GATE_VIDEO_DIR : $STAIRS_VIDEO_DIR;
+$video_dir .= "/$DATE/$HOUR";
+$all_videos = glob("$video_dir/*.mp4");
+foreach($all_videos as $index => $vid){
+    $vid_name = basename($vid);
+    echo "<h2><a href='./$CAMERA"."Videos/$DATE/$HOUR/$vid_name'>$vid_name</a> (54.55 MB)</h2>";
+}
+
+echo '</body></html>';
+?>
--- videos.php
+++ videos.php
@@ -11,11 +11,12 @@ arsort($date_dirs);
 
 echo '<html>';
 echo "<title>$CAMERA Videos ($DATE)</title>";
-echo "<head><h1><center><a href='../../'>GateVideos</a> ($DATE) </a></center></h1></head>";
+echo "<head><h1><center><a href='../../'>$CAMERA Videos</a> ($DATE) </a></center></h1></head>";
 echo '<body>';
 foreach($date_dirs as $k => $v){
     $hour = basename($v);
-    echo "<h2><A href='./21hour'>$hour</A> (4 Videos)</h2>";
+    $all_videos = glob("$v/*.mp4");
+    echo "<h2><A href='./video_preview.php?camera=$CAMERA&date=$DATE&hour=$hour'>$hour</A> (".count($all_videos)"." Videos)</h2>";
 }
 echo '</body>';
 echo '</html>';
