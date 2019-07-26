ccff0a1d4c6c9dea20ec9e4c572380f31aaca3da
Srinivas Gudla <gsrinivas37@gmail.com>
2019-07-25 20:27:51 -0400

    .

--- index.php
+++ index.php
@@ -8,62 +8,66 @@ $per_full = sprintf('%1.2f' , $per_full)
 ?>
 
 <html>
-    <head>
-        <title>Narahari Home CCTV Monitoring System</title>
-        <head><h1><center>Narahari Home CCTV Monitoring System</center></h1>
-            <center><b>Last Updated on : <?php echo date("Y-m-d h:i:sa")?>
-		&emsp;Storage: <?php echo "$per_full % Full, $free_space" ?> Available</b></center>
-        </head>
-        <style>
-            table, th, td {
-                border: 1px solid black;
-                border-collapse: collapse;
-            }
+<head>
+    <title>Narahari Home CCTV Monitoring System</title>
+    <head><h1><center>Narahari Home CCTV Monitoring System</center></h1>
+        <center><b>Last Updated on : <?php echo date("Y-m-d h:i:sa")?>
+                &emsp;Storage: <?php echo "$per_full % Full, $free_space" ?> Available</b></center>
+    </head>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
         th, td {
             padding: 5px;
             text-align: left;
         }
-        </style>
-    </head>
-    <body>
-        <br>
-        <br>
-        <table style=width:100%>
-            <tr>
-                <th><h2>Gate Photos</h2></th>
-                <th><h2>Stairs Photos</h2></th>
-                <th><h2>Gate Videos</h2></th>
-                <th><h2>Stairs Videos</h2></th>
-            </tr>
-	<?php
-	$dirs = array_filter(glob($GATE_PHOTO_DIR."/*"), 'is_dir');
-	arsort($dirs);
-	foreach($dirs as $k => $v){
-	    $v = basename($v);
-            echo '<tr>';
-		echo "<td><h2><a href='./photos.php?camera=Gate&date=$v'> $v </a></h2></td>";
-		if(file_exists("$STAIRS_PHOTO_DIR/$v")){
-		    echo "<td><h2><a href='./photos.php?camera=Stairs&date=$v'> $v </a></h2></td>";
-		}else{
-		    echo "<td><h2>Not Available</h2></td>";
-		}
-		if(file_exists("$GATE_VIDEO_DIR/$v")){
-		    echo "<td><h2><a href='./videos.php?camera=Gate&date=$v'> $v </a></h2></td>";
-		}else{
-		    echo "<td><h2>Not Available</h2></td>";
-    	}
-    	if(file_exists("$STAIRS_VIDEO_DIR/$v")){
-    	    echo "<td><h2><a href='./videos.php?camera=Stairs&date=$v'> $v </a></h2></td>";
-    	}else{
-    	    echo "<td><h2>Not Available</h2></td>";
+    </style>
+</head>
+<body>
+<br>
+<br>
+<table style=width:100%>
+    <tr>
+        <th><h2>Gate Photos</h2></th>
+        <th><h2>Stairs Photos</h2></th>
+        <th><h2>Gate Videos</h2></th>
+        <th><h2>Stairs Videos</h2></th>
+    </tr>
+    <?php
+    $dirs = array_filter(glob($GATE_PHOTO_DIR."/*"), 'is_dir');
+    arsort($dirs);
+    foreach($dirs as $k => $v){
+        $v = basename($v);
+        echo '<tr>';
+        echo "<td><h2><a href='./photos.php?camera=Gate&date=$v'> $v </a></h2></td>";
+        if(file_exists("$STAIRS_PHOTO_DIR/$v")){
+            echo "<td><h2><a href='./photos.php?camera=Stairs&date=$v'> $v </a></h2></td>";
+        }else{
+            echo "<td><h2>Not Available</h2></td>";
         }
-	    echo '</tr>';
-	}
-	?>
-        </table>
-        <h2><a href='./logs/internet_autologin/log.txt'> Internet Autologin</a></h2>
-        <h2><a href='./logs/nightly/log.txt'> Nightly Log </a></h2>
-        <h2><a href='./logs.php'> Logs </a></h2>
+        if(file_exists("$GATE_VIDEO_DIR/$v")){
+            echo "<td><h2><a href='./videos.php?camera=Gate&date=$v'> $v </a></h2></td>";
+        }else{
+            echo "<td><h2>Not Available</h2></td>";
+        }
+        if(file_exists("$STAIRS_VIDEO_DIR/$v")){
+            echo "<td><h2><a href='./videos.php?camera=Stairs&date=$v'> $v </a></h2></td>";
+        }else{
+            echo "<td><h2>Not Available</h2></td>";
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+    if(file_exists("$HDD_ROOT/logs/internt_autologin")){
+        echo "<h2><a href='./logs/internet_autologin/log.txt'> Internet Autologin</a></h2>";
+    }
+    if(file_exists("$HDD_ROOT/logs/nightly")){
+        echo "<h2><a href='./logs/nightly/log.txt'> Nightly Log </a></h2>";
+    }
+    ?>
+    <h2><a href='./logs.php'> Logs </a></h2>
 
-    </body>
+</body>
 </html>
