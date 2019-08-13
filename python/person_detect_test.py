62fefaec48fc0280789b3c50fbbf8a5a97dfd1d5
Srinivas Gudla <gsrinivas37@gmail.com>
2019-08-13 19:11:58 -0400

    .

--- python/person_detect_test.py
+++ python/person_detect_test.py
@@ -4,6 +4,7 @@ from shared import *
 from detector import *
 from detector import DetectorAPI
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input_file',type=argparse.FileType('r'),help="Image to be processed")
@@ -16,6 +17,9 @@ img_path = args.input_file.name
 odapi = DetectorAPI(path_to_ckpt=model_path)
 threshold = 0.5
 img = cv2.imread(img_path)
+
+start_time = time.time()
+
 boxes, scores, classes, num = odapi.processFrame(img)
 print("Processed image")
 label = ""
@@ -25,3 +29,7 @@ for i in range(len(boxes)):
     if classes[i] == 1 and scores[i] > threshold:
         label = label + "%d %d %d %d %s\n"%(box[0],box[1],box[2],box[3],str(round(scores[i]*100, 2)))
         print("Person found in image '%s': %s"%(img_path,label))
+
+total_time = time.time() - start_time
+str = ("Person detect took %d minutes and %d seconds\n")%(total_time/60, total_time%60)
+print(str)
