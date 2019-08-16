4c73b3e7ed3dc1f311f85238309058484a599796
Srinivas Gudla <gsrinivas37@gmail.com>
2019-08-16 19:29:36 -0400

    .

--- python/generate_tfrecord.py
+++ python/generate_tfrecord.py
@@ -103,9 +103,9 @@ def main(_):
     with contextlib2.ExitStack() as tf_record_close_stack:
   	    output_tfrecords = tf_record_creation_util.open_sharded_output_tfrecords(tf_record_close_stack, path, num_shards)
             for index, example in examples:
-            tf_example = create_tf_example(example)
-            output_shard_index = index % num_shards
-            output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
+                tf_example = create_tf_example(example)
+                output_shard_index = index % num_shards
+                output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
    
     
 
