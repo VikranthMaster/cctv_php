3f96c904724191528db5535031f3f61582e6771b
Srinivas Gudla <gsrinivas37@gmail.com>
2019-08-16 19:28:46 -0400

    .

--- python/generate_tfrecord.py
+++ python/generate_tfrecord.py
@@ -100,14 +100,13 @@ def main(_):
     
     num_shards=100
 	
-	with contextlib2.ExitStack() as tf_record_close_stack:
-  		output_tfrecords = tf_record_creation_util.open_sharded_output_tfrecords(tf_record_close_stack, path, num_shards)
-		for index, example in examples:
-    		tf_example = create_tf_example(example)
-    		output_shard_index = index % num_shards
-    		output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
-    
-    
+    with contextlib2.ExitStack() as tf_record_close_stack:
+  	    output_tfrecords = tf_record_creation_util.open_sharded_output_tfrecords(tf_record_close_stack, path, num_shards)
+            for index, example in examples:
+            tf_example = create_tf_example(example)
+            output_shard_index = index % num_shards
+            output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
+   
     
 
 if __name__ == '__main__':
