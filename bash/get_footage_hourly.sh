491c6ab8aa2fa0a19e1976c94bdc397f0d32cf82
Srinivas G <gsrinivas37@gmail.com>
2019-09-14 21:01:03 -0400

    .

--- bash/get_footage_hourly.sh
+++ bash/get_footage_hourly.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sudo service apache2 stop
 python3 /home/pi/cctv_php/python/get_footage.py &>> /mnt/hdd/logs/process/log_$(date +%F).txt
 python3 /home/pi/cctv_php/python/person_detect_us.py &>> /mnt/hdd/logs/person_detect/log_$(date +%F).txt
+sudo service apache2 start
--- php/train.php
+++ php/train.php
@@ -5,9 +5,26 @@ $CAMERA = $_GET["camera"];
 $DATE = $_GET["date"];
 $photo_dir = $CAMERA=="Gate"? $GATE_PHOTO_DIR : $STAIRS_PHOTO_DIR;
 $date_dirs = array_filter(glob($photo_dir."/$DATE/*"), 'is_dir');
-$train_file = $CAMERA=="Gate"? $HDD_ROOT."/training/GatePhotos/$DATE/train.txt" : $HDD_ROOT."/training/StairsPhotos/$DATE/train.txt" ;
+$train_file = $CAMERA."_".$DATE.".lbl";
 
 arsort($date_dirs);
+
+if(isset($_POST['submit'])){
+    if(file_exists($train_file)){
+	//Delete file
+	$de = unlink($train_file);
+    }
+    $fh = fopen($train_file, 'a');
+    if(!empty($_POST['check_list'])){
+	foreach($_POST['check_list'] as $selected){
+	    fwrite($fh, $selected."\n");
+	}
+    }
+    fclose($fh);
+    if(file_exists($train_file)){
+	echo "TRAINING FILE CREATED...";
+    }
+}
 ?>
 
 
@@ -16,16 +33,34 @@ arsort($date_dirs);
 <head>
     <meta charset="UTF-8">
     <title> <?php echo "$CAMERA Photos ($DATE)" ?> </title>
+    <style>
+.myButton {
+	background-color:#44c767;
+	-moz-border-radius:28px;
+	-webkit-border-radius:28px;
+	border-radius:28px;
+	border:1px solid #18ab29;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:22px;
+	padding:13px 20px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #2f6627;
+}
+.myButton:hover {
+	background-color:#5cbf2a;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
 
+    </style>
     <?php 
 	echo "<h1><center>";
 	echo "<a href='../../../'>$CAMERA Photos</a>&nbsp<a href='./photos.php?camera=$CAMERA&date=$DATE'>($DATE)</a>";
-	if(file_exists($train_file)){
-	    echo "&nbsp; <button onclick='update()'>UPDATE</button>";
-	}
-	else{
-	    echo "&nbsp; <button onclick='create()'>CREATE</button>";
-	}
 	echo "</center></h1>";
 
     ?>
@@ -36,6 +71,7 @@ $other_cam = $CAMERA=="Gate"? "Stairs" : "Gate";
 $today = strtotime($DATE);
 $yday = date('Y-m-d', strtotime('-1 day', $today ));
 $tmrw = date('Y-m-d', strtotime('1 day', $today));
+echo "<form action='#' method='post'>\n";
 echo '<h2>';
 if(file_exists("$photo_dir/$yday")){
     echo "<div style='float: left'><a href='./photos.php?camera=$CAMERA&date=$yday'> Previous</a> ($yday)</div>\n";
@@ -43,12 +79,25 @@ if(file_exists("$photo_dir/$yday")){
 if(file_exists("$photo_dir/$tmrw")) {
     echo "<div style='float: right'><a href='./photos.php?camera=$CAMERA&date=$tmrw'> Next</a> ($tmrw)</div>\n";
 }
-echo "<div style='margin: auto; width: 250px;'><a href='./photos.php?camera=$other_cam&date=$DATE'>$other_cam</a></div></h2>\n";
-
+echo "<div style='margin: auto; width: 250px;'><a href='./photos.php?camera=$other_cam&date=$DATE'>$other_cam</a> &nbsp;";
+if(file_exists($train_file)){
+    echo "<input type='submit' class='myButton' name='submit' value='Update'/>";
+}
+else{
+    echo "<input type='submit' class='myButton' name='submit' value='Create'/>";
+}
+echo "</div></h2>\n";
 foreach($date_dirs as $k => $v){
 $HOUR = basename($v);
 $photo_hour_dir = "$photo_dir/$DATE/$HOUR";
 $p_images = getPersonImages($photo_hour_dir);
+$files = "";
+if(file_exists($train_file)){
+    $myfile = fopen($train_file,'r');
+    $files = fread($myfile, filesize($train_file));
+    fclose($myfile);
+}
+    
 if(file_exists("$photo_hour_dir/person.txt")){
     $person_file = fopen("$photo_hour_dir/person.txt","r");
     $cam = $CAMERA=="Gate"?"GatePhotos":"StairsPhotos";
@@ -64,15 +113,19 @@ if(file_exists("$photo_hour_dir/person.txt")){
                 $img_link = $thumb_link;
             }
 	    $img_id = str_replace (".", "_", $f_name);
+	    if(file_exists($train_file) and strpos($files,$img_id)===false){
+		continue;
+	    }
 	    echo "<p id='$img_id'>\n";
             echo "<canvas id='$img_link' boxes='$boxes' width='640' height='360' style='border:1px solid #d3d3d3;'></canvas>\n";
-	    echo "<button onclick=\"remove('".$img_id."')\">Remove</button>\n";
+	    echo "<input onclick=\"remove('".$img_id."')\" type='checkbox' name='check_list[]' value='$img_id' checked><label>DELETE</label><br/>";
 	    echo "</p>\n";
         }
     }
     fclose($person_file);
 }
 }
+echo "</form>";
 ?>
 
 <script>
@@ -107,7 +160,7 @@ if(file_exists("$photo_hour_dir/person.txt")){
     function CreateFile(file){
 	// Requiring fs module in which 
 	// writeFile function is defined. 
-	const fs = require('fs') 
+	const fs = require('fs');
   
 	// Data which will write in a file. 
 	let data = "Learning how to write in a file."
