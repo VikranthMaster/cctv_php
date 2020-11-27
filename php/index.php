872e020a31f2bd4e5840875b95413cfea6febbca
Srinivas Gudla <gsrinivas37@gmail.com>
2020-11-27 04:55:52 -0500

    Update home

--- php/index.php
+++ php/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once('authorize.php');
 require 'vars.php';
+include ('conn.php');
+include ('queries.php');
 
 $free_space = HumanSize(disk_free_space($HDD_ROOT));
 $per_full = (1- disk_free_space($HDD_ROOT)/disk_total_space($HDD_ROOT))*100;
@@ -32,40 +34,27 @@ $per_full = sprintf('%1.2f' , $per_full)
     <tr>
         <th><h2>Gate Photos</h2></th>
         <th><h2>Stairs Photos</h2></th>
+        <th><h2>Gate Videos</h2></th>
+        <th><h2>Stairs Videos</h2></th>
+    </tr>
         <?php
-        echo "<th><h2>Gate Videos</h2></th>";
-        echo "<th><h2>Stairs Videos</h2></th>";
-        echo '</tr>';
-        $dirs = array_filter(glob($GATE_PHOTO_DIR."/*"), 'is_dir');
-        arsort($dirs);
-        foreach($dirs as $k => $v){
-            $v = basename($v);
-            echo '<tr>';
-            echo "<td><h2><a href='./photos.php?camera=Gate&date=$v'> $v </a></h2></td>";
-            if(file_exists("$STAIRS_PHOTO_DIR/$v")){
-                echo "<td><h2><a href='./photos.php?camera=Stairs&date=$v'> $v </a></h2></td>";
-            }else{
-                echo "<td><h2>Not Available</h2></td>";
-            }
-            echo "<td><h2><a href='./videos.php?camera=Gate&date=$v'> $v </a></h2></td>";
-            if(file_exists("$STAIRS_PHOTO_DIR/$v")){
-                echo "<td><h2><a href='./videos.php?camera=Stairs&date=$v'> $v </a></h2></td>";
-            }else{
-                echo "<td><h2>Not Available</h2></td>";
-            }
+    $result = mysqli_query($conn, $get_all_dates);
+    if (is_bool($result) && (mysqli_num_rows($result) == 0)) {
+        // print($error_msg, "Query ERROR: Failed to get summary data<br>" . __FILE__ ." line:". __LINE__ );
+    }
 
-            echo '</tr>';
-        }
-        echo '</table>';
-        if(file_exists("$HDD_ROOT/logs/internet_autologin")){
-            echo "<h2><a href='./logs/internet_autologin/log.txt'> Internet Autologin</a></h2>";
-        }
-        if(file_exists("$HDD_ROOT/logs/nightly")){
-            echo "<h2><a href='./logs/nightly/log.txt'> Nightly Log </a></h2>";
-        }
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         ?>
+   		<tr>
+			<td><h2><a href='./photos.php?camera=Gate&date=<?php echo $row["date"]; ?>'><?php echo $row["date"]; ?> </a></h2></td>
+			<td><h2><a href='./photos.php?camera=Gate&date=<?php echo $row["date"]; ?>'><?php echo $row["date"]; ?> </a></h2></td>
+			<td><h2><a href='./videos.php?camera=Gate&date=<?php echo $row["date"]; ?>'><?php echo $row["date"]; ?> </a></h2></td>
+			<td><h2><a href='./videos.php?camera=Gate&date=<?php echo $row["date"]; ?>'><?php echo $row["date"]; ?> </a></h2></td>
+		</tr>
+   
+   <?php } ?>
+        </table>
         <h2><a href='./logs.php'> Logs </a></h2>
         <h2><a href='./summary.php'> Summary </a></h2>
-
 </body>
 </html>
